06d2d35ee0c03b62bbd25ea6d755cce0329a4a52
BrysonSaclausa <brys1saclaus2@gmail.com>
2020-09-23 16:08:48 -0700

    unit tests complete

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -152,29 +152,9 @@ class GameAITests: XCTestCase {
         XCTAssertFalse(game(board: board, isWonBy: .o))
         XCTAssertFalse(game(board: board, isWonBy: .x))
     }
-    
-//    func testCatsGame() {
-//        /*
-//         x x o
-//         o o x
-//         x x o
-//         */
-//        var board = GameBoard()
-//        try! board.place(mark: .o, on: (2, 0))
-//        try! board.place(mark: .x, on: (0, 0))
-//        try! board.place(mark: .o, on: (1, 1))
-//        try! board.place(mark: .x, on: (1, 0))
-//        try! board.place(mark: .o, on: (0, 1))
-//        try! board.place(mark: .x, on: (2, 1))
-//        try! board.place(mark: .o, on: (2, 2))
-//        try! board.place(mark: .x, on: (1, 2))
-//        try! board.place(mark: .x, on: (0, 2))
-//        XCTAssertFalse(game(board: board, isWonBy: .o))
-//        XCTAssertFalse(game(board: board, isWonBy: .x))
-//         }
-//}
 
 
 
-     
+//
 }
+
